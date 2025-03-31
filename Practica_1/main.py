06a4d657e094bdc5b2dca13c496cf4b97d92e82e
Aitor <aitoribi100@gmail.com>
2025-03-31 13:18:59 +0200

    modifiacdo bien nn

--- Practica_1/main.py
+++ Practica_1/main.py
@@ -7,10 +7,12 @@ import cv2
 import numpy as np
 import glob
 import os
-
+import sys # to read argv
 # Dataset directory and image format
 DATA_DIR = "dataset"
 IMG_FORMAT = "/*.png"
+DATASET_MAX = 15 # maximum size of the dataset
+MIN_BATCH = 5 # minimum batch possible
 # Camera parameter directory
 PARAM_DIR = "params/"
 
@@ -29,26 +31,31 @@ objp[:, :2] = np.mgrid[0:CHESSBOARD_SIZE[0], 0:CHESSBOARD_SIZE[1]].T.reshape(-1,
 objpoints = []  # 3D world points
 imgpoints = []  # 2D image points
 
-def calculate_params(images):
+def calculate_params(images, dataset_max=DATASET_MAX):
     """ Calculates and saves the intrinsic and extrinsic parameters of the camera. """
     
     # Read the first image to get its size
     img = cv2.imread(images[0])
     height, width = img.shape[:2]
     image_shape = (width, height)  # OpenCV expects (width, height)
+    idx = 0 # counter of images included in the dataset
+    for fname in images: # per each image in the folder
+       if idx < dataset_max: # limits the dataset to check different Mean Projection Errors
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
 
-    for fname in images:
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
+            # Detect chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD_SIZE, None)
 
-        # Detect chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, CHESSBOARD_SIZE, None)
-
-        if ret:
-            objpoints.append(objp)  # Store 3D points
-            refined_corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)  
-            imgpoints.append(refined_corners)  # Store 2D points
-
+            if ret:
+                objpoints.append(objp)  # Store 3D points
+                refined_corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)  
+                imgpoints.append(refined_corners)  # Store 2D points
+       else:
+           break
+       idx = idx + 1 # update idx counter  
+          
+    print(f"Calculating camera calibration with {idx} images from the dataset")
     # Camera calibration
     ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(
         objpoints, imgpoints, image_shape, None, None
@@ -91,8 +98,10 @@ def compute_error(objpoints, imgpoints):
     return mean_error
 
 if __name__ == '__main__':
-    print("Calculating camera parameters...")
-
+    dataset_reg = False;
+    if len(sys.argv) > 1:
+        dataset_size = int(sys.argv[1])
+        dataset_reg = True
     # Get all image file paths
     images = glob.glob(DATA_DIR + IMG_FORMAT)
 
@@ -100,7 +109,11 @@ if __name__ == '__main__':
         print(" No images found in dataset directory!")
         exit()
 
-    # Perform calibration and compute error
-    img_points, obj_points = calculate_params(images)
+    # Perform calibration depending on dataset batch (if not is default 15)
+    if dataset_reg and dataset_size <= 15 and dataset_size >= MIN_BATCH:
+        img_points, obj_points = calculate_params(images, dataset_size)
+    else:
+        img_points, obj_points = calculate_params(images)
+    # Compute the Mean Reproyection Error
     error = compute_error(img_points, obj_points)
     print(f" Mean Reprojection Error: {error}")
