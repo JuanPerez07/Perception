ed550b92d150b9b06ac6e22d7ffe068bf2571576
Aitor <aitoribi100@gmail.com>
2025-03-31 16:20:37 +0200

    modificando params

--- Practica_2/main.py
+++ Practica_2/main.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 import numpy as np
+import sys
 from sklearn.cluster import DBSCAN
 
 POINTCLOUD_DIR = "clouds/scenes/"
@@ -8,7 +9,7 @@ OBJ_NAME = POINTCLOUD_DIR + CLOUD_NAME
 pcd = o3d.io.read_point_cloud(OBJ_NAME)
 OUTPUT_DIR = "clouds/scenes/"
 
-def remove_planes_using_clustering(pcd, eps=0.1, min_samples=50, max_planes=5):
+def remove_planes_using_clustering(pcd, eps=0.1, min_samples=50):
     """
     Removes dominant planes by clustering normals.
     """
@@ -18,14 +19,18 @@ def remove_planes_using_clustering(pcd, eps=0.1, min_samples=50, max_planes=5):
     # Normalize normals
     normals /= np.linalg.norm(normals, axis=1, keepdims=True)
     
+    print("Calculated Normals")
+    
     # Apply DBSCAN clustering based on normal similarity
-    clustering = DBSCAN(eps=eps, min_samples=min_samples, metric='cosine').fit(normals)
+    clustering = DBSCAN(eps=eps, min_samples=min_samples, metric='euclidean').fit(normals)
     labels, counts = np.unique(clustering.labels_, return_counts=True)
+    
+    print("Clustering and labeling done")
     
     # Sort clusters by size (most frequent = dominant planes)
     dominant_clusters = labels[np.argsort(-counts)]
     
-    for cluster in dominant_clusters[:max_planes]:
+    for cluster in dominant_clusters:
         if cluster == -1:  # Ignore noise points
             continue
         indices = np.where(clustering.labels_ == cluster)[0]
@@ -34,13 +39,23 @@ def remove_planes_using_clustering(pcd, eps=0.1, min_samples=50, max_planes=5):
     
     return remaining_pcd
 
-o3d.io.write_point_cloud(OUTPUT_DIR + "original.ply", pcd)
 
-# Estimate normals
-pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.05, max_nn=30))
+if __name__ == '__main__':
+    epsilon_reg  =  False
+    if len(sys.argv) > 1:
+        epsilon = int(sys.argv[1])
+        epsilon_reg = True
 
-# Apply clustering-based plane removal
-filtered_pcd = remove_planes_using_clustering(pcd)
+    #o3d.io.write_point_cloud(OUTPUT_DIR + "original.ply", pcd)
 
-o3d.visualization.draw_geometries([filtered_pcd], window_name="Filtered Point Cloud")
-o3d.io.write_point_cloud(OUTPUT_DIR + "resultados.ply", filtered_pcd)
+    # Estimate normals
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.05, max_nn=30))
+
+    # Apply clustering-based plane removal
+    if epsilon_reg:
+        filtered_pcd = remove_planes_using_clustering(pcd, epsilon)
+    else:
+        filtered_pcd = remove_planes_using_clustering(pcd)
+
+    #o3d.visualization.draw_geometries([filtered_pcd], window_name="Filtered Point Cloud")
+    o3d.io.write_point_cloud(OUTPUT_DIR + "epsilon" + epsilon + "resultados.ply", filtered_pcd)
