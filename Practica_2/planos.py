bd8eb3ff29a56fd91a02cbc4add1586065e49e16
Aitor <aitoribi100@gmail.com>
2025-04-14 16:03:47 +0200

    Main eliminado y solo dejo planos

--- Practica_2/planos.py
+++ Practica_2/planos.py
@@ -1,6 +1,6 @@
-import open3d as o3d
+import open3d as o3d # type: ignore
 import numpy as np
-from sklearn.cluster import DBSCAN
+#from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 
 
@@ -8,30 +8,32 @@ POINTCLOUD_DIR = "clouds/scenes/"
 CLOUD_NAME = "snap_0point.pcd"
 OBJ_NAME = POINTCLOUD_DIR + CLOUD_NAME
 OUTPUT_DIR = "clouds/scenes/"
-
+PLANOS= 3
 
 
 def remove_planes_using_ransac(pcd):
-    plane_model, inliers= pcd.segment_plane(distance_threshold=0.03,ransac_n=3,num_iterations=1000)
-    [a,b,c,d]= plane_model
-    print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
-    
-    inlier_cloud=pcd.select_by_index(inliers)
-    inlier_cloud.paint_uniform_color([1.0, 0, 0])
-    outlier_cloud = pcd.select_by_index(inliers, invert=True)
-    o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud],'Nube con plano')
-    
+    """ Eliminacion de nubes de puntos mediante ransac"""
+    for _ in range (PLANOS):
 
-
+        _, inliers= pcd.segment_plane(distance_threshold=0.03,ransac_n=5,num_iterations=1000)
+        
+        
+        
+        inlier_cloud=pcd.select_by_index(inliers)
+        #inlier_cloud.paint_uniform_color([1.0, 0, 0]) #Pintar los planos para verlos
+        outlier_cloud = pcd.select_by_index(inliers, invert=True)
+        #o3d.visualization.draw_geometries([outlier_cloud],'Nube sin el plano')
+        #o3d.visualization.draw_geometries([inlier_cloud,outlier_cloud],'Nube con el plano')
+        pcd= outlier_cloud
+    return pcd
 if __name__ == '__main__':
     
     # Ver nube de puntos original
-    
     pcd = o3d.io.read_point_cloud(OBJ_NAME)
     #o3d.visualization.draw_geometries([pcd],'Nube de puntos original')
+    #Eliminacion de planos mediante RANSAC
+    pcd= remove_planes_using_ransac(pcd)
 
-    remove_planes_using_ransac(pcd)
-
-    pcd = pcd.voxel_down_sample(voxel_size=0.01)
-
+    pcd = pcd.voxel_down_sample(voxel_size=0.005)
+    o3d.visualization.draw_geometries([pcd], 'Nube sin planos y con voxel')
    
