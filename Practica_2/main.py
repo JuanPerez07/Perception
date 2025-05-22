76a47c872f3618515dc4e6148ff8df04e2182ba5
Aitor <aitoribi100@gmail.com>
2025-05-22 18:45:45 +0200

    Based_on_feature y ICP

--- Practica_2/main.py
+++ Practica_2/main.py
@@ -75,9 +75,9 @@ def detect_keypoints_iss(pcd_scene, pcd_object):
     key_scene = o3d.geometry.keypoint.compute_iss_keypoints(
         pcd_scene,
         salient_radius=0.008,
-        non_max_radius=0.0085,
-        gamma_21=0.5,
-        gamma_32=0.5
+        non_max_radius=0.0065,
+        gamma_21=0.45,
+        gamma_32=0.45
     )
     # pintar los keypoints
     key_scene.paint_uniform_color([1, 0, 1])
@@ -89,37 +89,30 @@ def detect_keypoints_iss(pcd_scene, pcd_object):
     #o3d.io.write_point_cloud(f"{OUTPUT_DIR}scene_downsampled.ply", pcd_scene)
     #o3d.io.write_point_cloud(f"{OUTPUT_DIR}scene_keypoints.ply", key_scene)
 
-    # OBJETO: PIGGYBANK
-    piggy_pcd = pcd_object
-    # Estimar normales
-    piggy_pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=30))
+   
     
-    """ Calculo de keypoints para piggy 
-    key_piggy = o3d.geometry.keypoint.compute_iss_keypoints(
-        piggy_pcd,
-        salient_radius=0.008,
-        non_max_radius=0.008,
-        gamma_21=0.5,
-        gamma_32=0.5
-    )
-    """
-    key_piggy = o3d.geometry.keypoint.compute_iss_keypoints(
-        piggy_pcd,
+    # Estimar normales
+    pcd_object.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=0.01, max_nn=30))
+    
+   
+    key_obj = o3d.geometry.keypoint.compute_iss_keypoints(
+        pcd_object,
         salient_radius=0.008,#radio de vecindad
-        non_max_radius=0.0055,#filtro para que no estén super cerca
-        gamma_21=0.69,#cambios en la curvatura
-        gamma_32=0.69#cambio de curvatura en otra direccion
+        non_max_radius=0.006,#filtro para que no estén super cerca
+        gamma_21=0.6,#cambios en la curvatura
+        gamma_32=0.6,#cambio de curvatura en otra direccion
+        min_neighbors=4
     )
     #o3d.io.write_point_cloud(f"{OBJ_DIR}piggy_kp_iss.ply", key_piggy)
     
 
     print("Keypoints detected with ISS for scene and object")
 
-    key_piggy.paint_uniform_color([1, 0, 1])
-    piggy_pcd.paint_uniform_color([0, 0.5, 0.5])
-    #o3d.visualization.draw_geometries([key_piggy,piggy_pcd],'Key de figura')
+    key_obj.paint_uniform_color([1, 0, 1])
+    pcd_object.paint_uniform_color([0, 0.5, 0.5])
+    #o3d.visualization.draw_geometries([key_obj,obj_pcd],'Key de figura')
     # Return the scene and object keypoints
-    return key_scene, key_piggy
+    return key_scene, key_obj
 
 
 """
@@ -130,7 +123,7 @@ def descript_fpfh(key, pcd, voxel_size=0.005):
 
     # Estimar normales si no existen
     if not pcd.has_normals():
-        pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=15))
+        pcd.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=30))
 
     # Calcular FPFH para toda la nube
     fpfh = o3d.pipelines.registration.compute_fpfh_feature(
@@ -145,43 +138,36 @@ def descript_fpfh(key, pcd, voxel_size=0.005):
         [_, idx, _] = pcd_tree.search_knn_vector_3d(kp, 1)
         indices.append(idx[0])
 
-    keypoint_fpfh = fpfh.data[:, indices]
-    return keypoint_fpfh
+    data_all=np.asarray(fpfh.data)
+    filtered_data=data_all[:,indices]
+    fpfh_keypoints=o3d.pipelines.registration.Feature()
+    fpfh_keypoints.data=filtered_data
+   
+   
+    return fpfh_keypoints
 
 """
 Compute the matching between descriptors using KDTreeFlann and RANSAC
 """
-def matching(desc_scene, desc_obj, key_scene, key_obj, max_dist=0.025):
-    desc_scene_np = np.asarray(desc_scene).T
-    desc_obj_np = np.asarray(desc_obj).T
+def matching(keypoints_scene, keypoints_obj, desc_scene, desc_obj,vx_size=0.025):
 
-    kdtree = o3d.geometry.KDTreeFlann(desc_scene_np.T)
 
-    correspondences = []
-    nn = 5 # max nearest neighbours searched for matching
-    for i, desc in enumerate(desc_obj_np):
-        [_, idx, _] = kdtree.search_knn_vector_xd(desc, nn)
-        correspondences.append([i, idx[0]])
+    max_dist=vx_size*1.5
+    angle=np.deg2rad(38)
 
-    corres = o3d.utility.Vector2iVector(correspondences)
-
-    # guardar los matching en un .ply
-    exportar_correspondencias_a_obj(key_obj, key_scene, corres)
-    # params ajustar correspondencias
-    edge_length = 0.45
-    normal_angle_thres = math.pi / 10 # algo degrees
-    distance_threshold = 0.16
-    result = o3d.pipelines.registration.registration_ransac_based_on_correspondence(
-        key_obj,  # objeto = source
-        key_scene,  # escena = target
-        corres,
-        max_correspondence_distance=max_dist,
+    result = o3d.pipelines.registration.registration_ransac_based_on_feature_matching(
+        source=keypoints_obj,
+        target=keypoints_scene,
+        source_feature=desc_obj,
+        target_feature=desc_scene,
+        mutual_filter=True,  # Para que las correspondencias sean mutuas
+        max_correspondence_distance=vx_size,
         estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(False),
-        ransac_n=3,
+        ransac_n=5,
         checkers=[
-            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(edge_length),
-            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(distance_threshold),
-            o3d.pipelines.registration.CorrespondenceCheckerBasedOnNormal(normal_angle_thres)
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnEdgeLength(0.65),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnDistance(0.16),
+            o3d.pipelines.registration.CorrespondenceCheckerBasedOnNormal(angle)
         ],
         criteria=o3d.pipelines.registration.RANSACConvergenceCriteria(4000000, 500)
     )
@@ -221,6 +207,20 @@ def visualizar_correspondencias(kp_obj, kp_scene, correspondences):
 """
 Guardar matches para visualizar con CloudCompare
 """
+
+def refine_registration_icp(source, target, init_transform, voxel_size=0.005):
+    distance_threshold = voxel_size *1.5 # o prueba con voxel_size * 2
+    
+    icp_result = o3d.pipelines.registration.registration_icp(
+        source,
+        target,
+        max_correspondence_distance=distance_threshold,
+        init=init_transform,
+        estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint(),
+        criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=50)
+    )
+    
+    return icp_result
 def exportar_correspondencias_a_obj(kp_obj, kp_scene, correspondences, file_path=OUTPUT_DIR+"correspondencias_lines.obj"):
     obj_points = np.asarray(kp_obj.points)
     scene_points = np.asarray(kp_scene.points)
@@ -261,7 +261,7 @@ def insertar_objeto_en_escena(scene_pcd, obj_pcd, transformation_matrix):
 
 if __name__ == '__main__':
     # load both scene and objects pcds
-    piggy_pcd = o3d.io.read_point_cloud(PLANT) # object
+    obj_pcd = o3d.io.read_point_cloud(PIGGY) # object
     og_scene_pcd = o3d.io.read_point_cloud(ORIGINAL_CLOUD) # scene 
     # o3d.visualization.draw_geometries([pcd], 'Nube de puntos original')
 
@@ -269,26 +269,28 @@ if __name__ == '__main__':
     vx_size = 0.005
     scene_pcd = downsample_pcd(og_scene_pcd, vx_size)
 
+
     # Remove the main planes of the scene to reduce computational load
     scene_pcd = remove_planes_using_ransac(scene_pcd)
  
     
-    #piggy_pcd = downsample_pcd(piggy_pcd, vx_size)
+   
 
     # o3d.visualization.draw_geometries([pcd_scene], 'Nube sin planos y con voxel')
     o3d.io.write_point_cloud(f"{OUTPUT_DIR}original_sin_planos.ply", scene_pcd)
     #o3d.visualization.draw_geometries([scene_pcd],'Nube de puntos cambiado')
 
     # Compute the keypoints for scene and object
-    kp_scene, kp_obj = detect_keypoints_iss(scene_pcd,piggy_pcd)
+    kp_scene, kp_obj = detect_keypoints_iss(scene_pcd,obj_pcd)
     # Compute the decriptors for scene keypoints and obj keypoints using FPFH
     scene_desc = descript_fpfh(kp_scene, scene_pcd)
     print("Descriptors calculated for scene")
-    obj_desc = descript_fpfh(kp_obj, piggy_pcd)
+    obj_desc = descript_fpfh(kp_obj, obj_pcd)
     print("Descriptors calculated for object")
     # Realizar matching entre los descriptores usando KDTree junto a RANSAC para filtrar
-    match_result = matching(scene_desc, obj_desc, kp_scene, kp_obj) # incluye matriz de transformacion R|t
+    match_result = matching(kp_scene,kp_obj,scene_desc,obj_desc) # incluye matriz de transformacion R|t
     print("Matching done with KDTreeFlann and RANSAC")
+    refined=refine_registration_icp(obj_pcd,scene_pcd,match_result.transformation,vx_size)
     # nube de puntos de la escena con el objeto detectado
-    insertar_objeto_en_escena(og_scene_pcd, piggy_pcd, match_result.transformation)
+    insertar_objeto_en_escena(og_scene_pcd, obj_pcd, refined.transformation)
     print("Program successfully terminated")
